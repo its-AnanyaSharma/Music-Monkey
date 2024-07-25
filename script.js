22db5e53792afffcdbdb6a93154d2980109869a3
Ananyaa <147981527+its-AnanyaSharma@users.noreply.github.com>
2024-07-25 20:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`https://raw.githubusercontent.com/its-AnanyaSharma/huraaayy/main/songs/${folder}/info.json`)
+
+    // let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
